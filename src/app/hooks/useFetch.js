da12c7c139302e5cfcb595413ea3fe2023888af7
chituenguyen <tuenguyen553@gmail.com>
2023-07-28 14:26:06 +0700

    done handle logic coding

--- src/app/hooks/useFetch.js
+++ src/app/hooks/useFetch.js
@@ -4,18 +4,18 @@ import { useQuery } from "react-query";
 
 const API_URL = "https://apisf.p2pcdn.xyz/api/v1/unique-tournament/17/season/41886/statistics"
 
-const fetchTournamentStatistics = (page,orderGoal) => {
+const fetchTournamentStatistics = (page,orderGoal,group) => {
   return axios.get(
-    `${API_URL}?limit=20&order=${orderGoal}&accumulation=total&group=attack&offset=${
+    `${API_URL}?limit=20&order=${orderGoal}&accumulation=total&group=${group}&offset=${
       20 * (page - 1)
     }`
   );
 };
 
-const useTournamentStatistics = (page,orderGoal) => {
+const useTournamentStatistics = (page,orderGoal,group) => {
   return useQuery({
-    queryKey:['tournamentStatistics',page,orderGoal],
-    queryFn:()=>fetchTournamentStatistics(page,orderGoal)
+    queryKey:['tournamentStatistics',page,orderGoal, group],
+    queryFn:()=>fetchTournamentStatistics(page,orderGoal, group)
   }); // Wrap the fetch call in a function
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,10 @@
 import React, { useState } from "react";
 
 import useTournamentStatistics from "./hooks/useFetch";
-import Typography from "@mui/material/Typography";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 
-const columns = [
+const columnsSummary = [
   {
     header: "ID",
     accessorKey: "player.id",
@@ -20,12 +19,16 @@ const columns = [
     accessorKey: "goals",
   },
   {
+    header: "Mục tiêu dự kiến",
+    accessorKey: "expectedGoals",
+  },
+  {
     header: "Rê bóng thành công",
     accessorKey: "successfulDribbles",
   },
   {
     header: "Tắc bóng",
-    accessorKey: "bigChancesMissed",
+    accessorKey: "tackles",
   },
   {
     header: "Kiến tạo",
@@ -33,7 +36,7 @@ const columns = [
   },
   {
     header: "Tỉ lệ chuyền bóng thành công",
-    accessorKey: "passCompletionPercentage", // Replace 'passCompletionPercentage' with the correct key for pass completion percentage if available in your data.
+    accessorKey: "accuratePassesPercentage", // Replace 'accuratePassesPercentage' with the correct key for pass completion percentage if available in your data.
   },
   {
     header: "Điểm số",
@@ -41,17 +44,150 @@ const columns = [
   },
 ];
 
+const columnsAttack = [
+  {
+    header: "ID",
+    accessorKey: "player.id",
+  },
+  {
+    header: "Cầu thủ",
+    accessorKey: "player.name", // Access the player's name
+  },
+  {
+    header: "Bàn thắng",
+    accessorKey: "goals",
+  },
+  {
+    header: "Cơ hội lớn bỏ lỡ",
+    accessorKey: "bigChancesMissed",
+  },
+  {
+    header: "Rê bóng thành công",
+    accessorKey: "successfulDribbles",
+  },
+  {
+    header: "Số cú sút",
+    accessorKey: "totalShots",
+  },
+  {
+    header: "Phần trăm chuyển hoá cơ hội",
+    accessorKey: "goalConversionPercentage", // Replace 'accuratePassesPercentage' with the correct key for pass completion percentage if available in your data.
+  },
+  {
+    header: "Điểm số",
+    accessorKey: "rating",
+  },
+];
+const columnsDefence = [
+  {
+    header: "ID",
+    accessorKey: "player.id",
+  },
+  {
+    header: "Cầu thủ",
+    accessorKey: "player.name", // Access the player's name
+  },
+  {
+    header: "Tắc bóng",
+    accessorKey: "tackles",
+  },
+  {
+    header: "Số lần đánh chặn",
+    accessorKey: "interceptions",
+  },
+  {
+    header: "Phá bóng",
+    accessorKey: "clearances",
+  },
+  {
+    header: "Phạm lỗi dẫn đến bàn thắng",
+    accessorKey: "errorLeadToGoal",
+  },
+  {
+    header: "Điểm số",
+    accessorKey: "rating",
+  },
+];
+const columnsPassing = [
+  {
+    header: "ID",
+    accessorKey: "player.id",
+  },
+  {
+    header: "Cầu thủ",
+    accessorKey: "player.name", // Access the player's name
+  },
+  {
+    header: "Cơ hội lớn tạo ra",
+    accessorKey: "bigChancesCreated",
+  },
+  {
+    header: "Kiến tạo",
+    accessorKey: "assists",
+  },
+  {
+    header: "Số đường chuyền chính xác",
+    accessorKey: "accuratePasses",
+  },
+  {
+    header: "Tỉ lệ chuyền bóng chính xác",
+    accessorKey: "accuratePassesPercentage",
+  },
+  {
+    header: "Số đường chuyền quyết định",
+    accessorKey: "keyPasses",
+  },
+  {
+    header: "Điểm số",
+    accessorKey: "rating",
+  },
+];
+const columnsGoalKeeper = [
+  {
+    header: "ID",
+    accessorKey: "player.id",
+  },
+  {
+    header: "Cầu thủ",
+    accessorKey: "player.name", // Access the player's name
+  },
+  {
+    header: "Giữ sạch lưới",
+    accessorKey: "cleanSheet",
+  },
+  {
+    header: "Cản phá Penalty",
+    accessorKey: "penaltySave",
+  },
+  {
+    header: "Tình huống cứu thua trong box",
+    accessorKey: "savedShotsFromInsideTheBox",
+  },
+  {
+    header: "Ra khỏi vị trí",
+    accessorKey: "runsOut",
+  },
+];
 const IndexPage = () => {
   const [page, setPage] = useState(1);
   const [sorted, setSorted] = useState("-goals");
+  const [group, setGroup] = useState("Summary");
+  const groupHeader = [
+    { id: 1, name: "Summary" },
+    { id: 2, name: "Attack" },
+    { id: 3, name: "Defence" },
+    { id: 4, name: "Passing" },
+    { id: 5, name: "Goalkeeper" },
+  ];
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    event.preventDefault();
     setPage(value);
   };
   const {
     data: apiResponse,
     isLoading,
     error,
-  } = useTournamentStatistics(page, sorted); // Use the custom hook
+  } = useTournamentStatistics(page, sorted, group.toLowerCase()); // Use the custom hook
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -59,43 +195,45 @@ const IndexPage = () => {
     return <div>Error</div>;
   }
   const handleColumnClick = (header: string) => {
-    if (header === "goals") {
-      if (sorted === "-goals") {
-        setSorted("goals");
-      } else {
-        setSorted("-goals");
-      }
-    } else if (header === "rating") {
-      if (sorted === "-rating") {
-        setSorted("rating");
-      } else {
-        setSorted("-rating");
-      }
-    }
-    else if(header === "successfulDribbles"){
-      if (sorted === "-successfulDribbles") {
-        setSorted("successfulDribbles");
-      } else {
-        setSorted("-successfulDribbles");
-      }
-    }
-    else if (header === "bigChancesMissed"){
-      if(sorted === "-bigChancesMissed"){
-        setSorted("bigChancesMissed")
-      }
-      else{
-        setSorted("-bigChancesMissed")
-      }
+    if (sorted === "-" + header) {
+      setSorted(header);
+    } else {
+      setSorted("-" + header);
     }
     setPage(1);
   };
+  const handleGroupClick = (name: string) => {
+    setGroup(name);
+  };
+  const selectedColumns =
+    group === "Summary"
+      ? columnsSummary
+      : group === "Attack"
+      ? columnsAttack
+      : group === "Defence"
+      ? columnsDefence
+      : group === "Passing"
+      ? columnsPassing
+      : columnsGoalKeeper;
+
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Simple Table Example</h1>
+      <h1 className="text-3xl font-bold mb-4">Player stats</h1>
+      <div className="flex items-center">
+        {groupHeader.map((item) => (
+          <button
+            key={item.id}
+            className={group === item.name ? "button-active" : "button"}
+            onClick={() => handleGroupClick(item.name)}
+          >
+            {item.name}
+          </button>
+        ))}
+      </div>
       <table className="w-full table-auto border-collapse">
         <thead>
           <tr className="bg-gray-100">
-            {columns.map((column, id) => (
+            {selectedColumns.map((column, id) => (
               <th
                 key={id}
                 className="border px-4 py-2 hover:cursor-pointer"
@@ -110,7 +248,7 @@ const IndexPage = () => {
           {apiResponse?.data.results.map((row: any, id: number) => {
             return (
               <tr key={id} className="text-center">
-                {columns.map((column) => {
+                {selectedColumns.map((column) => {
                   const accessorKeys = column.accessorKey.split("."); // Split the accessorKey by '.' to access nested properties
                   let cellData = row;
                   accessorKeys.forEach((key) => {
